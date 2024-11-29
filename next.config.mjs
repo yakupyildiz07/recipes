ef934e1864981d5bc24c97dc606b35fa6002055b
yakupyildiz07 <yakup4235@gmail.com>
2024-11-29 15:37:55 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['www.themealdb.com'],
   },
+  output: 'export', 
+  basePath: '/recipes', 
+  assetPrefix: '/recipes'
 };
 
 export default nextConfig;
